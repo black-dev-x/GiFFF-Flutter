d29a4165d151dc6e61a4912019231d0e44a2abbc
thiagodevx <thiago.dev.x@gmail.com>
2020-07-12 17:38:10 -0300

    classes

--- orientacao_objetos/classe_data.dart
+++ orientacao_objetos/classe_data.dart
@@ -4,6 +4,11 @@ class Data {
   int ano;
 
   Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
+  Data.com({this.dia = 1, this.mes = 1, this.ano = 1});
+  Data.ultimoDiaDoAno(this.ano) {
+    dia = 31;
+    mes = 12;
+  }
 
   String toString() {
     return "$dia/$mes/$ano";
@@ -16,4 +21,7 @@ main() {
 
   Data dataCompra = Data(12, 7, 2020);
   print(dataCompra);
+
+  var dataFinal = Data.com(dia: 12, mes: 7, ano: 2024);
+  print(dataFinal);
 }
