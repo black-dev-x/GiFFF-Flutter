71537f1ca2c4696aec3c1f6bc07d7384f4c381ac
thiagodevx <thiago.dev.x@gmail.com>
2020-09-07 13:39:07 -0300

    utilizando controllers

--- expenses_manager/lib/expenses/expenses_form.dart
+++ expenses_manager/lib/expenses/expenses_form.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class ExpensesForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -11,15 +14,20 @@ class ExpensesForm extends StatelessWidget {
           children: <Widget>[
             TextField(
               decoration: InputDecoration(labelText: 'Title:'),
+              controller: titleController,
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Value \$:'),
+              controller: valueController,
             ),
             Row(
               children: <Widget>[
                 FlatButton(
                   child: Text('Register Expense'),
-                  onPressed: () => {},
+                  onPressed: () {
+                    print(titleController.text);
+                    print(valueController.text);
+                  },
                   textColor: Colors.purple,
                 ),
               ],
